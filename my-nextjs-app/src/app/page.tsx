4894521a3c159e482678e1055c6bd197a6421ce4
AkumaNoUtsuwa <karlmacasusi717@gmail.com>
2025-11-27 09:58:39 +0800

    port

--- my-nextjs-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MainSection from "@/Components/Sections/Main_Sec";
-export default function Home() {
-
-    return (
-        <div className='text-center'>
-            <MainSection/>
-        </div>
-    );
-}
